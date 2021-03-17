ccbe22d66f7cd79fe2d3c5eaac04c98359fc2e40
foxamanda2 <fox.amanda2@gmail.com>
2021-03-17 12:43:31 -0400

    New Form with state tracking input fields

--- ClientApp/src/pages/NewReview.jsx
+++ ClientApp/src/pages/NewReview.jsx
@@ -1,10 +1,74 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function NewReviewModal(props) {
-  return <div className="review-modal">You pressed me</div>
+  const [summary, setSummary] = useState('')
+  const [body, setBody] = useState('')
+
+  function submitNewReview(event) {
+    event.preventDefault()
+
+    console.log(`Make new thing ${summary} ${body}`)
+  }
+
+  return (
+    <div className="review-modal">
+      <form className="newReview">
+        <p>New Review</p>
+
+        <fieldset>
+          <input
+            name="summary"
+            type="text"
+            placeholder="Title"
+            value={summary}
+            onChange={function (event) {
+              setSummary(event.target.value)
+            }}
+          />
+        </fieldset>
+
+        <fieldset>
+          <input
+            name="body"
+            type="text"
+            placeholder="Review"
+            value={body}
+            onChange={function (event) {
+              setBody(event.target.value)
+            }}
+          />
+        </fieldset>
+
+        <fieldset>
+          <div className="stars">
+            <span style={{ '--rating': 4.7 }}></span>(2)
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <div>Created at</div>
+        </fieldset>
+
+        <fieldset>
+          <Link to="#">
+            <button className="submit" onClick={submitNewReview}>
+              Submit
+            </button>
+          </Link>
+        </fieldset>
+
+        <fieldset>
+          <a href="/" className="home">
+            Home
+          </a>
+        </fieldset>
+      </form>
+    </div>
+  )
 }
 export function NewReview() {
-  const [userPressedNew, setUserPressedNew] = useState(false)
+  const [userPressedNew, setUserPressedNew] = useState(true)
 
   return (
     <>
